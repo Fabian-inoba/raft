093397053bdff2c42e2274b66a5118cdd1189b75
BarrelDevelopment <aayi.ijay@gmail.com>
2025-12-07 13:10:27 +0100

    Update CompGridCalendarYear.cs
    Fix calendar rendering

--- raft/components/CompGridCalendarYear.cs
+++ raft/components/CompGridCalendarYear.cs
@@ -1,4 +1,7 @@
+using System.Globalization;
 using Spectre.Console;
+using Calendar = Spectre.Console.Calendar;
+
 namespace raft.components;
 
 public class CompGridCalendarYear {
@@ -6,18 +9,18 @@ public class CompGridCalendarYear {
     private const int GridRows = 3;
     
     private List<Calendar> calendars = new List<Calendar>();
-    
+
     public Grid CalendarGird { get; set; }
     
     public CompGridCalendarYear(int year) {
-        
+
         CalendarGird = new Grid();
         CalendarGird.AddColumns(GridColumns);
         
         for (int cnt = 1; cnt <= 12; cnt++) {
-            calendars.Add(new Calendar(year, cnt));
+            calendars.Add(new Calendar(year, cnt){ Culture = CultureInfo.CurrentCulture });
         }
-        
+
         CalendarGird.AddRow(calendars[0], calendars[1], calendars[2], calendars[3]);
         CalendarGird.AddRow(calendars[4], calendars[5], calendars[6], calendars[7]);
         CalendarGird.AddRow(calendars[8], calendars[9], calendars[10], calendars[11]);
