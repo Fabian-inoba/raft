8e1d7bb27393842ad0952aab1ee427f3fe495de5
BarrelDevelopment <aayi.ijay@gmail.com>
2025-12-07 14:23:00 +0100

    Create CompUserInput.cs
    Create user input component to handle user input hot keys

---
+++ raft/components/CompUserInput.cs
@@ -0,0 +1,76 @@
+namespace raft.components;
+
+public class CompUserInput {
+    public enum InputType {
+        NextCalendarDay,            // RightArrow
+        NextCalendarMonth,          // SHIFT+RightArrow
+        NextCalendarYear,           // CTL+SHIFT+RightArrow
+        PreviousCalendarDay,        // LeftArrow
+        PreviousCalendarMonth,      // SHIFT+LeftArrow
+        PreviousCalendarYear,       // CTL+SHIFT+LeftArrow
+        StatisticMonth,             // ALT+M
+        StatisticYear,              // ALT+Y
+        SwitchProfile,              // ALT+P
+        EditProfileInfo,            // ALT+E
+        SaveData,                   // CTL+S
+        Exit,                       // ESC
+        None
+    }
+    
+    public InputType ReadUserInput() {
+        Console.TreatControlCAsInput = true;
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+        ConsoleKey key = keyInfo.Key;
+        ConsoleModifiers mod = keyInfo.Modifiers;
+
+        if (mod.HasFlag(ConsoleModifiers.Control) && mod.HasFlag(ConsoleModifiers.Shift))
+        {
+            return key switch
+            {
+                ConsoleKey.RightArrow => InputType.NextCalendarYear,
+                ConsoleKey.LeftArrow  => InputType.PreviousCalendarYear,
+                _ => InputType.None
+            };
+        }
+
+        if (mod.HasFlag(ConsoleModifiers.Shift))
+        {
+            return key switch
+            {
+                ConsoleKey.RightArrow => InputType.NextCalendarMonth,
+                ConsoleKey.LeftArrow  => InputType.PreviousCalendarMonth,
+                _ => InputType.None
+            };
+        }
+
+        if (mod.HasFlag(ConsoleModifiers.Control))
+        {
+            return key switch
+            {
+                ConsoleKey.S => InputType.SaveData,
+                _ => InputType.None
+            };
+        }
+
+        if (mod.HasFlag(ConsoleModifiers.Alt))
+        {
+            return key switch
+            {
+                ConsoleKey.M => InputType.StatisticMonth,
+                ConsoleKey.Y => InputType.StatisticYear,
+                ConsoleKey.P => InputType.SwitchProfile,
+                ConsoleKey.E => InputType.EditProfileInfo,
+                _ => InputType.None
+            };
+        }
+
+        return key switch
+        {
+            ConsoleKey.LeftArrow  => InputType.PreviousCalendarDay,
+            ConsoleKey.RightArrow => InputType.NextCalendarDay,
+            ConsoleKey.Escape     => InputType.Exit,
+            _ => InputType.None
+        };
+    }
+}
