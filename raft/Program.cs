25ae63aeeaf039d3635dfd8370bddbda30737510
BarrelDevelopment <aayi.ijay@gmail.com>
2025-12-09 16:00:56 +0100

    Change to async and implement ui manger as test
    User input is working. Change to async

--- raft/Program.cs
+++ raft/Program.cs
@@ -1,4 +1,5 @@
 ﻿using raft.components;
+using raft.Managers;
 using Spectre.Console;
 using Spectre.Console.Rendering;
 
@@ -6,33 +7,30 @@ namespace raft;
 // Welcome to RAFT RAFT RAFT (Redeem allowance and fuel tracker)
 class Program {
 
-    public static void Main(string[] args) {
+    public static async Task Main(string[] args) {
         
         //TODO: Add check if the terminal is in a certain size. Inform 
         //user if not and force user to change size
         Console.SetWindowSize(500, 500);
-
-        CompUserInput.InputType inputType = CompUserInput.InputType.None;
-        CompLayout layout = new CompLayout();
         
-        CompGridCalendarYear calendarYear = new CompGridCalendarYear(DateTime.Now.Year);
-
-        if (layout.Layout == null) {
-            throw new Exception("Layout is null");
-        }
-
+        UiManager.Init();
+        
+        
+        CompUserInput.InputType inputType = CompUserInput.InputType.None;
         CompUserInput userInput = new CompUserInput();
         
-        AnsiConsole.Live(layout.Layout)
-            .Start(ctx => {
+        
+        await AnsiConsole.Live(UiManager.MainLayout.Layout)
+            .StartAsync(async ctx => {
                 while (inputType != CompUserInput.InputType.Exit) {
-                    inputType = userInput.ReadUserInput();
-                    layout.UpdateContent(CompLayout.Section.statistics, new Panel(
+                    inputType = userInput.GetUserInput();
+                    UiManager.MainLayout.UpdateContent(CompLayout.Section.statistics, new Panel(
                         Align.Center(
                             new Markup($"Your input [blue]{inputType}[/]"),
                             VerticalAlignment.Middle)));
-                    layout.UpdateContent(CompLayout.Section.calendar, calendarYear.CalendarGird);
+                    UiManager.MainLayout.UpdateContent(CompLayout.Section.calendar, UiManager.CalendarYearGird.CalendarGird);
                     ctx.Refresh();
+                    await Task.Delay(TimeSpan.FromSeconds(1));
                 }
             });
         
